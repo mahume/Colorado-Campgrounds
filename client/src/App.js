db6277d8d95f20103c8c23c91fc70638ce11d08c
mahume <michael.a.hume@gmail.com>
2019-05-15 18:21:38 -0600

    Adding href page routes to Nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div>
+        <Nav />
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,12 +7,10 @@ function Nav() {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-          <a className="nav-item nav-link" href="#">Features</a>
-          <a className="nav-item nav-link" href="#">Pricing</a>
-          <a className="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
+          <a className="nav-item nav-link" href="/login">Login</a>
         </div>
       </div>
     </nav>
