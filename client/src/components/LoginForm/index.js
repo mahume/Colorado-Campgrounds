ea6ed33e9496894e6206505a35047388c0bdcc95
mahume <michael.a.hume@gmail.com>
2019-05-20 17:52:58 -0600

    React-Bootstrap Form

---
+++ client/src/components/LoginForm/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { Group, Label, Control, Text, Check } from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function LoginForm() {
+  return (
+    <Container>
+      <Form>
+        <Group controlId="formBasicEmail">
+          <Label>Email address</Label>
+          <Control type="email" placeholder="Enter email" />
+          <Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Text>
+        </Group>
+        <Group controlId="formBasicPassword">
+          <Label>Password</Label>
+          <Control type="password" placeholder="Password" />
+        </Group>
+        <Group controlId="formBasicChecbox">
+          <Check type="checkbox" label="Check me out" />
+        </Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default LoginForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Map from '../components/Map';
 import QueryList from '../components/QueryList';
-import { request } from 'http';
 
 class Home extends Component {
   state = {};
