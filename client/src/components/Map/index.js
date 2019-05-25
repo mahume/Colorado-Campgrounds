1e4834812abef5bcb8cb4c0a7659b13b76125f09
mahume <michael.a.hume@gmail.com>
2019-05-25 12:05:30 -0600

    dotenv, .env, map key

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
 class Map extends Component {
   static defaultProps = {
     center: {
@@ -17,7 +16,7 @@ class Map extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+import dotenv from 'dotenv';
 import App from './App';
 
 // style
 import 'spectre.css/dist/spectre.min.css';
 import 'spectre.css/dist/spectre-icons.css';
+
+dotenv.config()
 
 ReactDOM.render(
   <BrowserRouter>
