2ecca704eeb5fb837ab4943d8ece55e8cee3c92b
mahume <michael.a.hume@gmail.com>
2019-05-27 12:40:27 -0600

    QueryList scrolling capability

--- client/src/components/QueryList/index.js
+++ client/src/components/QueryList/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import QueryItem from '../QueryItem';
+import './style.css'
 
 function QueryList() {
   return (
-    <ListGroup as="ul">
+    <ListGroup id="list-group" as="ul">
+      <QueryItem as="li"></QueryItem>
       <QueryItem as="li"></QueryItem>
       <QueryItem as="li"></QueryItem>
       <QueryItem as="li"></QueryItem>
