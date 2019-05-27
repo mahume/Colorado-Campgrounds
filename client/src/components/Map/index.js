8a2edd85f5caaec24ab4b4dedabb041eb71b3387
mahume <michael.a.hume@gmail.com>
2019-05-27 12:34:02 -0600

    Map container, height change

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Container from 'react-bootstrap/Container';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class Map extends Component {
@@ -13,20 +14,23 @@ class Map extends Component {
  
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
+      <Container>
+        {/* Important! Always set the container height explicitly */}
+        <div style={{ height: '50vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+        
+      </Container>
     );
   }
 }
