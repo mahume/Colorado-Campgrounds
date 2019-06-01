5b4b39520b7343eeefb61e00ca7588434f023043
mahume <michael.a.hume@gmail.com>
2019-06-01 13:28:07 -0600

    adds / to concatted path

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session()); // calls the deserializeUser
 
 // Routes
-app.use(express.static(path.join(__dirname + 'client/build')))
+app.use(express.static(path.join(__dirname + '/client/build')))
 app.use('/', routes);
 
 // Starting Server
